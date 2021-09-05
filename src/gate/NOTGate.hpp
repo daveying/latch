7356e386e071cb00b8b7000c8322c1bfecbb26d6
daveying <daveying@163.com>
2021-09-05 22:16:31 +0800

    Add NOT gate

---
+++ src/gate/NOTGate.hpp
@@ -0,0 +1,35 @@
+#ifndef NOT_GATE_HPP__
+#define NOT_GATE_HPP__
+
+#include <ZeroDelayPin.hpp>
+#include <GateTemplate.hpp>
+#include <TruthTable.hpp>
+
+namespace gate
+{
+
+// ID = 5, InputSize = 1, OutputSize = 1
+static constexpr GateID NOTGateID         = 5;
+static constexpr size_t NOTGateInputSize  = 1;
+static constexpr size_t NOTGateOutputSize = 1;
+DEFINE_GATE_TRUTH_TABLE(NOTGate, NOTGateID, NOTGateInputSize, NOTGateOutputSize)
+{
+    if (input[0]->value() == PinState::Low)
+    {
+        output[0]->value(PinState::High);
+    }
+    else
+    {
+        output[0]->value(PinState::Low);
+    }
+}
+END_GATE_TRUTH_TABLE(NOTGate, NOTGateID, NOTGateInputSize, NOTGateOutputSize)
+
+using NOTGate = GateTemplate<
+    TypePack<ZeroDelayInputPin>,
+    gate_info<NOTGateID>::TruthTable,
+    TypePack<ZeroDelayOutputPin>>;
+
+} // namespace gate
+
+#endif // NOR_GATE_HPP__
---
+++ tests/components/NOTGate/NOTGate.cpp
@@ -0,0 +1,25 @@
+#include <NOTGate.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(NOTGate, DefaultState)
+{
+    gate::NOTGate notGate;
+    EXPECT_EQ(notGate.input(0)->value(), gate::PinState::Low);
+    EXPECT_EQ(notGate.output(0)->value(), gate::PinState::High);
+}
+
+TEST(NOTGate, TruthTable)
+{
+    gate::NOTGate notGate;
+
+    auto in  = notGate.input(0);
+    auto out = notGate.output(0);
+
+    // in = Low
+    EXPECT_EQ(out->value(), gate::PinState::High);
+
+    // in = High
+    in->value(gate::PinState::High);
+    EXPECT_EQ(out->value(), gate::PinState::Low);
+}
