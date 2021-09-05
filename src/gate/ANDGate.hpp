4b58e82bcb9959ddc44d96c3cbec05ead8644f2c
daveying <daveying@163.com>
2021-09-05 22:07:15 +0800

    Add AND gate

---
+++ src/gate/ANDGate.hpp
@@ -0,0 +1,35 @@
+#ifndef AND_GATE_HPP_
+#define AND_GATE_HPP_
+
+#include <ZeroDelayPin.hpp>
+#include <GateTemplate.hpp>
+#include <TruthTable.hpp>
+
+namespace gate
+{
+
+// ID = 4, InputSize = 2, OutputSize = 1
+static constexpr GateID ANDGateID         = 4;
+static constexpr size_t ANDGateInputSize  = 2;
+static constexpr size_t ANDGateOutputSize = 1;
+DEFINE_GATE_TRUTH_TABLE(ANDGate, ANDGateID, ANDGateInputSize, ANDGateOutputSize)
+{
+    if (input[0]->value() == PinState::High && input[1]->value() == PinState::High)
+    {
+        output[0]->value(PinState::High);
+    }
+    else
+    {
+        output[0]->value(PinState::Low);
+    }
+}
+END_GATE_TRUTH_TABLE(ANDGate, ANDGateID, ANDGateInputSize, ANDGateOutputSize)
+
+using ANDGate = GateTemplate<
+    TypePack<ZeroDelayInputPin, ZeroDelayInputPin>,
+    gate_info<ANDGateID>::TruthTable,
+    TypePack<ZeroDelayOutputPin>>;
+
+} // namespace gate
+
+#endif // AND_GATE_HPP_
---
+++ tests/components/ANDGate/ANDGate.cpp
@@ -0,0 +1,33 @@
+#include <ANDGate.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(ANDGate, DefaultState)
+{
+    gate::ANDGate andGate;
+    EXPECT_EQ(andGate.input(0)->value(), gate::PinState::Low);
+    EXPECT_EQ(andGate.input(1)->value(), gate::PinState::Low);
+    EXPECT_EQ(andGate.output(0)->value(), gate::PinState::Low);
+}
+
+TEST(ANDGate, TruthTable)
+{
+    gate::ANDGate andGate;
+
+    auto in0 = andGate.input(0);
+    auto in1 = andGate.input(1);
+    auto out = andGate.output(0);
+
+    // in0 = Low, in1 = Low
+    EXPECT_EQ(out->value(), gate::PinState::Low);
+
+    // in0 = High, in1 = Low
+    in0->value(gate::PinState::High);
+    EXPECT_EQ(out->value(), gate::PinState::Low);
+    // in0 = High, in1 = High
+    in1->value(gate::PinState::High);
+    EXPECT_EQ(out->value(), gate::PinState::High);
+    // in0 = Low, in1 = High
+    in0->value(gate::PinState::Low);
+    EXPECT_EQ(out->value(), gate::PinState::Low);
+}
