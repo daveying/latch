dddea020be219e56311aabb39b8e3714ade45536
daveying <daveying@163.com>
2021-09-05 18:05:21 +0800

    SR latch tested with gtest

--- tests/sr-latch/main.cpp
+++ tests/sr-latch/main.cpp
@@ -1,39 +1,55 @@
 #include <iostream>
 #include <NORGate.hpp>
 
-int main(int argc, const char** argv)
+#include <gtest/gtest.h>
+
+TEST(SRLatch, LatchTruthTable)
 {
     gate::NORGate norGate0;
     gate::NORGate norGate1;
+    EXPECT_EQ(norGate0.output(0)->value(), gate::PinState::High);
+    EXPECT_EQ(norGate1.output(0)->value(), gate::PinState::High);
     norGate0.output(0)->connect(norGate1.input(0));
+    EXPECT_EQ(norGate1.output(0)->value(), gate::PinState::Low);
     norGate1.output(0)->connect(norGate0.input(1));
+    EXPECT_EQ(norGate0.output(0)->value(), gate::PinState::High);
 
-    // S High
-    norGate0.input(0)->value(gate::PinState::High);
-    // R Low
-    norGate1.input(1)->value(gate::PinState::Low);
+    auto S  = norGate0.input(0);
+    auto R  = norGate1.input(1);
+    auto Qb = norGate0.output(0);
+    auto Q  = norGate1.output(0);
+
+    // S -> High
+    S->value(gate::PinState::High);
+    // R -> Low
+    R->value(gate::PinState::Low);
     // S High, R Low
-    std::cout << "S High, R Low" << std::endl;
-    std::cout << static_cast<int>(norGate0.output(0)->value()) << std::endl;
-    std::cout << static_cast<int>(norGate1.output(0)->value()) << std::endl;
+    EXPECT_EQ(gate::PinState::Low, Qb->value());
+    EXPECT_EQ(gate::PinState::High, Q->value());
 
-    // S Low
-    norGate0.input(0)->value(gate::PinState::Low);
+    // S -> Low
+    S->value(gate::PinState::Low);
     // S Low, R Low
-    std::cout << "S Low, R Low" << std::endl;
-    std::cout << static_cast<int>(norGate0.output(0)->value()) << std::endl;
-    std::cout << static_cast<int>(norGate1.output(0)->value()) << std::endl;
-    // R High
-    norGate1.input(1)->value(gate::PinState::High);
+    EXPECT_EQ(gate::PinState::Low, Qb->value());
+    EXPECT_EQ(gate::PinState::High, Q->value());
+
+    // R -> High
+    R->value(gate::PinState::High);
     // S Low, R High
-    std::cout << "S Low, R High" << std::endl;
-    std::cout << static_cast<int>(norGate0.output(0)->value()) << std::endl;
-    std::cout << static_cast<int>(norGate1.output(0)->value()) << std::endl;
-    // S High
-    norGate0.input(0)->value(gate::PinState::High);
+    EXPECT_EQ(gate::PinState::High, Qb->value());
+    EXPECT_EQ(gate::PinState::Low, Q->value());
+
+    // R -> Low
+    R->value(gate::PinState::Low);
+    EXPECT_EQ(gate::PinState::High, Qb->value());
+    EXPECT_EQ(gate::PinState::Low, Q->value());
+
+    // R -> High
+    R->value(gate::PinState::High);
+    // S -> High
+    S->value(gate::PinState::High);
     // S High, R High
-    std::cout << "S High, R High" << std::endl;
-    std::cout << static_cast<int>(norGate0.output(0)->value()) << std::endl;
-    std::cout << static_cast<int>(norGate1.output(0)->value()) << std::endl;
-    return 0;
+    // EXPECT_EQ(gate::PinState::Low, Qb->value());
+    // EXPECT_EQ(gate::PinState::Low, Q->value());
 }
+
