02a01f187fd5a9f2d71b3c768b915e11d1de0c9d
daveying <daveying@163.com>
2022-08-28 18:40:38 +0800

    Add descriptor TS loader

--- tools/viewer/frontend/src/descriptor/loader.ts
+++ tools/viewer/frontend/src/descriptor/loader.ts
@@ -1,3 +1,47 @@
-export default function sum(a: number, b: number) {
-    return a + b;
+/////////////////////////////////////////////////////////////////////////////////
+// MIT License
+//
+// Copyright (c) 2021 Xingpeng Da
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/////////////////////////////////////////////////////////////////////////////////
+
+export enum PinType {
+    ZeroDelayInputPin,
+    ZeroDelayOutputPin,
+    DelayedInputPin_1,
+    DelayedInputPin_5,
+    UnitInertialInputPin,
+    ForwardInputPin,
+    ForwardOutputPin,
+}
+
+export class Pin {
+    type: PinType;
+    constructor(type: PinType) {
+        this.type = type;
+    }
+}
+export class Gate {
+    inputPins: Pin[];
+    outputPins: Pin[];
+    constructor(inputPins: Pin[], outputPins: Pin[]) {
+        this.inputPins = inputPins;
+        this.outputPins = outputPins;
+    }
 }
