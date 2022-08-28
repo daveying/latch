f67cb999c67d6b58e67c237dcbf7af8b38f27ffb
daveying <daveying@163.com>
2022-08-28 18:40:38 +0800

    Add descriptor TS loader

--- tools/viewer/frontend/tests/foo.test.ts
+++
@@ -1,5 +0,0 @@
-import sum from "../src/descriptor/loader"
-
-test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-});
---
+++ tools/viewer/frontend/tests/loader.test.ts
@@ -0,0 +1,5 @@
+import { PinType } from "../src/descriptor/loader"
+
+test("Defined PinType", () => {
+    expect(PinType.DelayedInputPin_1).toBe(PinType.DelayedInputPin_1);
+});
