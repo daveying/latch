60ee1c5712deb5e0ae1208953b61726dfeba4c9d
daveying <daveying@163.com>
2022-09-07 22:21:43 +0800

    Add test for checking the existance of the descriptor file

--- tools/viewer/frontend/tests/loader.test.ts
+++ tools/viewer/frontend/tests/loader.test.ts
@@ -1,4 +1,18 @@
 import { PinType } from "../src/descriptor/loader";
+import fs from "fs";
+
+describe("Make sure the descriptor file is ready for loader tests", () => {
+    test("Env var DESCRIPTOR_PATH is set", () => {
+        expect(process.env.DESCRIPTOR_PATH).toBeTruthy();
+    });
+
+    test("Can open component descriptor file", () => {
+        fs.readFile(process.env.DESCRIPTOR_PATH as string, (err, data) => {
+            expect(err).toBeFalsy();
+            expect(data).toBeTruthy();
+        });
+    });
+});
 
 test("Defined PinType", () => {
     expect(PinType.DelayedInputPin_1).toBe(PinType.DelayedInputPin_1);
