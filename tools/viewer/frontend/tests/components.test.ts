4b124537d38805d388bad5f831aa6d3462e76292
daveying <daveying@163.com>
2022-09-27 22:32:55 +0800

    Add getComponent API

--- tools/viewer/frontend/tests/components.test.ts
+++ tools/viewer/frontend/tests/components.test.ts
@@ -1,3 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////////
+// MIT License
+//
+// Copyright (c) 2022 Xingpeng Da
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/////////////////////////////////////////////////////////////////////////////////
+
 import {
     Loader,
     ComponentDescriptor,
@@ -132,5 +155,46 @@ describe("Component can be constructed from Loader.", () => {
         pin0.peers.push(pin1);
         expect(() => pin0.connect(pin1)).toThrow("Program is in corrupted state");
         expect(() => pin1.connect(pin0)).toThrow("Program is in corrupted state");
+    });
+});
+
+describe("Component.getComponent works correctly.", () => {
+    let loader = Loader.fromFile(process.env.DESCRIPTOR_PATH as string);
+    function createComponent(type: string): Component {
+        let desc = loader.getDescriptor(type);
+        if (!desc) {
+            throw `Cannot find descriptor: '${type}'.`;
+        }
+        return Component.fromDescriptor(type.toLowerCase(), null, desc, loader.descriptors);
+    }
+    test("getComponent can get itself.", () => {
+        let alu32 = createComponent("ALU32");
+        let itself = alu32.getComponent("alu32");
+        expect(itself).toBe(alu32);
+        let negater15 = alu32.getComponent("alu32.negater[15]");
+        expect(negater15?.getComponent("alu32.negater[15]")).toBe(negater15);
+    });
+    test("getComponent works.", () => {
+        let alu32 = createComponent("ALU32");
+        let adder31 = alu32.getComponent("alu32.adder[31]");
+        expect(adder31).toBeTruthy();
+        expect(adder31?.pins.length).toBe(5);
+
+        let xor0 = adder31?.getComponent("alu32.adder[31].xor0");
+        expect(xor0).toBeTruthy();
+        expect(xor0?.subcomponents.length).toBe(0);
+        expect(xor0?.id()).toBe("alu32.adder[31].xor0");
+        expect(xor0?.pins.length).toBe(3);
+    });
+    test("getComponent return null when id invalid.", () => {
+        let register16 = createComponent("Register16");
+        let bit7 = register16.getComponent("register16.bit[7]");
+        expect(bit7).toBeTruthy();
+
+        expect(bit7?.getComponent("")).toBeNull();
+        expect(bit7?.getComponent("register16.bit[7].abc")).toBeNull();
+        expect(bit7?.getComponent("register16.bit[7].")).toBeNull();
+        expect(bit7?.getComponent("register16.bit[7]..")).toBeNull();
+        expect(bit7?.getComponent(".register16.bit[7]")).toBeNull();
     });
 });
